c7e921ec30fbf44b3003c7fa3815cbcdee45fd16
pure-soul-kk <krishnakripa34567@gmail.com>
2023-06-12 12:36:55 +0530

    DRG: Add vendorsetup.sh & remove.txt for CLI based builds

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+# Common Tree
+rm -rf device/nokia/sdm660-common
+git clone https://github.com/ProjectElixir-Devices/device_nokia_sdm660-common-PL2.git device/nokia/sdm660-common
+
+# Vendor Tree
+rm -rf vendor/nokia/DRG
+git clone https://github.com/ProjectElixir-Devices/proprietary_vendor_nokia_DRG.git vendor/nokia/DRG
+
+# Common Vendor Tree
+rm -rf vendor/nokia/sdm660-common
+git clone https://github.com/ProjectElixir-Devices/proprietary_vendor_nokia_sdm660-common.git vendor/nokia/sdm660-common
+
+# Kernel Tree
+rm -rf kernel/nokia/sdm660
+git clone https://github.com/ProjectElixir-Devices/kernel_nokia_sdm660.git kernel/nokia/sdm660
+
+# Clang
+rm -rf prebuilts/clang/kernel/linux-x86/clang-r416183b
+git clone https://github.com/pure-soul-kk/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git prebuilts/clang/kernel/linux-x86/clang-r416183b
